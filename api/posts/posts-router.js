fbda93e01be320c37d47224ebbdb291fd2c902b4
HanselV <hanselviva@gmail.com>
2021-07-10 13:53:29 -0700

    skeleton finished, working on endpoints now

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,29 @@
 // implement your posts router here
+
+// BASIC IMPORTS HERE =======
+const express = require("express");
+// router:
+const router = express.Router();
+//data:
+const Post = require("./posts-model.js");
+// ==========
+
+//we use router. instead of server.
+// then we write how we normally would write handlers
+
+// GET - Returns an array of all the post objects contained in the database
+router.get("/", (req, res) => {
+	Post.find()
+		.then((posts) => {
+			res.status(200).json(posts);
+		})
+		.catch((err) => {
+			res.status(500).json({
+				error: err,
+				message: "Error retrieving posts",
+			});
+		});
+});
+
+// don't forget to export!
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,29 @@
 // implement your server here
 // require your posts router and connect it here
+
+//
+//
+const express = require("express");
+
+const server = express();
+server.use(express.json());
+
+// Dividing our API into different routers to keep out code organized as the app grows. That is why we write our routers into separate files.
+
+// This is how you "import" a router:
+const postsRouter = require("./posts/posts-router");
+
+// Defining your import:
+server.use("/api/posts", postsRouter);
+
+// This middleware is not bound to any URL prefix because it is a "default" router in Express() app - a constant called "server" in our instance here.
+server.get("/", (req, res) => {
+	res.send(`
+    <h1> Hello World! </h1>
+    <p> This is Express Routing </p>
+    `);
+});
+// Thus, this handler will match "GET /" requests.
+
+// exporting:
+module.exports = server;
