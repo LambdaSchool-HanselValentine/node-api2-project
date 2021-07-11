1a2227fedf2fbc7ac177e54cb3119a3dda07ac4b
HanselV <hanselviva@gmail.com>
2021-07-10 20:02:16 -0700

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -32,7 +32,7 @@ router.get("/:id", (req, res) => {
 	Post.findById(id)
 		.then((post) => {
 			post
-				? res.status(200).json(post)
+				? res.status(200).send(post)
 				: res.status(404).json({
 						message: "The post with the specified ID does not exist",
 				  });
@@ -45,8 +45,51 @@ router.get("/:id", (req, res) => {
 });
 
 // POST - Creates a post using the information sent inside the request body and returns the newly created post object
+router.post("/", (req, res) => {
+	const postData = req.body;
+
+	if (!postData.title || !postData.contents) {
+		res.status(400).json({
+			message: "Please provide title and contents for the post",
+		});
+	} else {
+		Post.insert(postData)
+			.then((post) => {
+				res.status(201).send(post);
+			})
+			.catch(() => {
+				res.status(500).json({
+					message: "There was an error while saving the post to the database",
+				});
+			});
+	}
+});
 
 // PUT - Updates the post with the specified id using data from the request body and returns the modified document, not the original
+router.put("/:id", (req, res) => {
+	const { id } = req.params;
+	const newData = req.body;
+
+	if (!newData.title || !newData.contents) {
+		res.status(400).json({
+			message: "Please provide title and contents for the post",
+		});
+	} else {
+		Post.update(id, newData)
+			.then((updatedData) => {
+				updatedData
+					? res.status(200).json(updatedData)
+					: res.status(404).json({
+							message: "The post with the specified ID does not exist",
+					  });
+			})
+			.catch(() => {
+				res.status(500).json({
+					message: "The post information could not be modified",
+				});
+			});
+	}
+});
 
 // DELETE - Removes the post with the specified id and returns the deleted post object
 
