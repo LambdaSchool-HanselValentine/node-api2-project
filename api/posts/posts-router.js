fdc04f08e17386be7cd959b664c6f63598b9b2f2
HanselV <hanselviva@gmail.com>
2021-07-10 18:40:42 -0700

    2 endpoints done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 // implement your posts router here
 
 // BASIC IMPORTS HERE =======
@@ -17,13 +18,39 @@ router.get("/", (req, res) => {
 		.then((posts) => {
 			res.status(200).json(posts);
 		})
-		.catch((err) => {
-			res.status(500).json({
-				error: err,
-				message: "Error retrieving posts",
-			});
+		.catch(() => {
+			res
+				.status(500)
+				.json({ message: "The posts information could not be retrieved" });
 		});
 });
+
+// GET - Returns the post object with the specified id
+router.get("/:id", (req, res) => {
+	const id = req.params.id;
+
+	Post.findById(id)
+		.then((post) => {
+			post
+				? res.status(200).json(post)
+				: res.status(404).json({
+						message: "The post with the specified ID does not exist",
+				  });
+		})
+		.catch(() => {
+			res
+				.status(500)
+				.json({ message: "The post information could not be retrieved" });
+		});
+});
+
+// POST - Creates a post using the information sent inside the request body and returns the newly created post object
+
+// PUT - Updates the post with the specified id using data from the request body and returns the modified document, not the original
+
+// DELETE - Removes the post with the specified id and returns the deleted post object
+
+// GET - Returns an array of all the comment objects associated with the post with the specified id
 
 // don't forget to export!
 module.exports = router;
